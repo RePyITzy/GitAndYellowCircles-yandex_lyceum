0f4778984b93ca7f0ba10423bfa2f5049eab42d6
RePyITzy <127877297+RePyITzy@users.noreply.github.com>
2023-11-18 23:31:12 +0300

    Update main.py
    Новая версия "Git и желтые окружности"
    - Теперь программа полностью работает.

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5 import uic
 
 from random import randint
 import sys
 
 
-class Ui_MyGitWidget(object):
-    def setupUi(self, MyGitWidget):
-        if not MyGitWidget.objectName():
-            MyGitWidget.setObjectName(u"MyGitWidget")
-        
-        MyGitWidget.resize(755, 528)
-        self.centralwidget = QWidget(MyGitWidget)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.drawButton = QPushButton(self.centralwidget)
-        self.drawButton.setObjectName(u"drawButton")
-        self.drawButton.setGeometry(QRect(10, 490, 111, 23))
-        MyGitWidget.setCentralWidget(self.centralwidget)
-      
-        self.retranslateUi(MyGitWidget)
-        QMetaObject.connectSlotsByName(MyGitWidget)
-
-    def retranslateUi(self, MyGitWidget):
-        MyGitWidget.setWindowTitle(QCoreApplication.translate("MyGitWidget", u"MainWindow", None))
-        self.drawButton.setText(QCoreApplication.translate("MyGitWidget", u"\u041d\u0430\u0440\u0438\u0441\u043e\u0432\u0430\u0442\u044c", None))
-
-
 class YellowCircles(QMainWindow, Ui_MyGitWidget):
     def __init__(self):
         super(YellowCircles, self).__init__()
-        self.setupUi(self)
-        self.retranslateUi(self)
+        uic.loadUi('UI.ui')
 
         self.go_draw = False
 
@@ -45,12 +24,11 @@ class YellowCircles(QMainWindow, Ui_MyGitWidget):
         if self.go_draw:
             self.go_draw = False
             width, height = self.geometry().width(), self.geometry().height()
-            random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
             random_position = [randint(0, width), randint(0, height)]
             random_diameter = randint(64, 90)
 
             painter = QPainter(self)
-            painter.setBrush(QColor(*random_color))
+            painter.setBrush(Qt.Yellow)
             painter.drawEllipse(*random_position, random_diameter, random_diameter)
 
         return super(YellowCircles, self).paintEvent(event)
